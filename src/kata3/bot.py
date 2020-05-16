c0074360761770a8c3f4d32958596db742f46d9d
Jose Fuster <jofuspei@MacBook-Pro-de-Jose-2.local>
2020-05-16 18:47:24 +0200

    kata 3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,20 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    return update.message.reply_text("Hola, Geeks!")
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    return update.message.reply_text("Ayudame!")
 
 def mayus(update, context):
         #
-        return ""
+        return update.message.reply_text(' '.join(context.args).upper())
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
     #
-    return ""
+    return update.message.reply_text(update.message.text[::-1])
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -34,18 +34,18 @@ def error(update, context):
 def main():
     """Inicio del Bot"""
     #Colocamos el Token creado por FatherBot
-    updater = Updater("", use_context=True)
+    updater = Updater("1181213813:AAHWNXrQnmYQ1wriw8q1aRYdCUSdM0uTQEA", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_handler(MessageHandler(Filters.command, alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)
